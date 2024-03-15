13cec80f1e90f1b8e25966ace11c8d880350ee22
charleseMuchmore <87589934+charleseMuchmore@users.noreply.github.com>
2024-03-14 17:40:45 -0700

    Revert "Account"

---
+++ BudgetApp/BudgetApp/Models/Category.cs
@@ -0,0 +1,7 @@
+﻿namespace BudgetApp.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+    }
+}
--- BudgetApp/Models/AccountVM.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BudgetApp.Models
-{
-    public class AccountVM
-    {
-        public IFormFile Image { get; set; }
-    }
-}
